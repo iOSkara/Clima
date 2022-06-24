5697ceac2d408777b88446b273ea27b92cebf038
Roman Vasyltsov <rgamer402@gmail.com>
2022-06-24 23:19:30 +0300

    UI is done

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Mac on 23.06.2022.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
